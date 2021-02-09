e260bc94ef42109725061cce9a54f7e070ce6bbd
Sjuul Janssen <sjuul@outlook.com>
2021-02-09 18:40:29 +0100

    lest stricter versions

--- versions.tf
+++ versions.tf
@@ -3,14 +3,14 @@ terraform {
 
   required_providers {
     aws = {
-      version = "~> 3.5.0"
+      version = "=> 3.5.0"
     }
     kubectl = {
       source  = "gavinbunney/kubectl"
       version = "~> 1.6.2"
     }
     kubernetes = {
-      version = "~> 1.13"
+      version = "=> 1.13"
     }
   }
 }
