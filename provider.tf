497602d6731b9bf88e6ad4a9be1b6592753a318f
Sjuul Janssen <sjuul@outlook.com>
2021-05-19 15:18:24 +0200

    added provider file

---
+++ provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = ">= 0.13"
+  required_providers {
+    aws        = {
+      version = ">= 3.5.0"
+    }
+    kubectl    = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.7.0"
+    }
+    kubernetes = {
+      version = ">= 1.13"
+    }
+  }
+}
