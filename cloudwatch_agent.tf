2775858141a2f98b910a42bf501ec67f93b0a8d7
Sjuul Janssen <sjuul@outlook.com>
2021-05-19 15:32:04 +0200

    split yaml files

--- cloudwatch_agent.tf
+++ cloudwatch_agent.tf
@@ -5,7 +5,7 @@ resource "kubernetes_config_map" "cwagentconfig" {
     name      = "cwagentconfig"
     namespace = kubernetes_namespace.amazon-cloudwatch.metadata[0].name
   }
-  data = {
+  data       = {
     "cwagentconfig.json" = <<EOT
 {
     "agent": {
@@ -33,26 +33,26 @@ EOT
 }
 
 resource "aws_iam_role" "cwagent-eks" {
-  count = var.enable_cloudwatch_agent ? 1 : 0
-  name  = "cwagent-eks"
+  count              = var.enable_cloudwatch_agent ? 1 : 0
+  name               = "cwagent-eks"
   assume_role_policy = jsonencode(
-    {
-      Statement = [
-        {
-          Action = "sts:AssumeRoleWithWebIdentity"
-          Condition = {
-            StringEquals = {
-              "${var.oidc_host_path}:aud" = "sts.amazonaws.com"
-            }
+  {
+    Statement = [
+      {
+        Action    = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "${var.oidc_host_path}:aud" = "sts.amazonaws.com"
           }
-          Effect = "Allow",
-          Principal = {
-            Federated = "arn:aws:iam::${var.account_id}:oidc-provider/${var.oidc_host_path}"
-          }
-        },
-      ]
-      Version = "2012-10-17"
-    }
+        }
+        Effect    = "Allow",
+        Principal = {
+          Federated = "arn:aws:iam::${var.account_id}:oidc-provider/${var.oidc_host_path}"
+        }
+      },
+    ]
+    Version   = "2012-10-17"
+  }
   )
 }
 
@@ -62,18 +62,28 @@ resource "aws_iam_role_policy_attachment" "cwagent-eks" {
   policy_arn = aws_iam_policy.eks-cloudwatch-policy.arn
 }
 
-
-resource "kubectl_manifest" "cwagent-serviceaccount" {
-  count      = var.enable_cloudwatch_agent ? 1 : 0
-  depends_on = [kubernetes_namespace.amazon-cloudwatch, kubernetes_config_map.cwagentconfig[0]]
-  yaml_body = templatefile("${path.module}/yamls/cwagent-serviceaccount.yaml", {
+resource "template_file" "cwagent-serviceaccount" {
+  count    = var.enable_cloudwatch_agent ? 1 : 0
+  filename = "${path.module}/yamls/cwagent-serviceaccount.yaml"
+  vars     = {
     account_id          = var.account_id,
     cloudwatch_iam_role = aws_iam_role.cwagent-eks[0].name,
-  })
+  }
+}
+
+resource "file" "cwagent-daemonset" {
+  count    = var.enable_cloudwatch_agent ? 1 : 0
+  filename = "${path.module}/yamls/cwagent-daemonset.yaml"
+}
+
+resource "kubectl_manifest" "cwagent-serviceaccount" {
+  depends_on = [kubernetes_namespace.amazon-cloudwatch, kubernetes_config_map.cwagentconfig[0]]
+  for_each   = split("---", file.cwagent-daemonset)
+  yaml_body  = each.value
 }
 
 resource "kubectl_manifest" "cwagent-daemonset" {
-  count      = var.enable_cloudwatch_agent ? 1 : 0
+  for_each = split("---", file(file().))
   depends_on = [kubernetes_namespace.amazon-cloudwatch, kubectl_manifest.cwagent-serviceaccount[0], kubernetes_config_map.cwagentconfig[0], aws_iam_role_policy_attachment.cwagent-eks[0]]
-  yaml_body  = file("${path.module}/yamls/cwagent-daemonset.yaml")
+  yaml_body  = each.value
 }
--- fluentd.tf
+++ fluentd.tf
@@ -1,24 +1,24 @@
 resource "aws_iam_role" "fluentd-cloudwatch" {
-  count = var.enable_logs_forwarding ? 1 : 0
-  name  = "fluentd-cloudwatch"
+  count              = var.enable_logs_forwarding ? 1 : 0
+  name               = "fluentd-cloudwatch"
   assume_role_policy = jsonencode(
-    {
-      Statement = [
-        {
-          Action = "sts:AssumeRoleWithWebIdentity"
-          Condition = {
-            StringEquals = {
-              "${var.oidc_host_path}:aud" = "sts.amazonaws.com"
-            }
-          }
-          Effect = "Allow"
-          Principal = {
-            Federated = "arn:aws:iam::${var.account_id}:oidc-provider/${var.oidc_host_path}"
+  {
+    Statement = [
+      {
+        Action    = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "${var.oidc_host_path}:aud" = "sts.amazonaws.com"
           }
         }
-      ]
-      Version = "2012-10-17"
-    }
+        Effect    = "Allow"
+        Principal = {
+          Federated = "arn:aws:iam::${var.account_id}:oidc-provider/${var.oidc_host_path}"
+        }
+      }
+    ]
+    Version   = "2012-10-17"
+  }
   )
 }
 
@@ -28,11 +28,17 @@ resource "aws_iam_role_policy_attachment" "fluentd-cloudwatch" {
   policy_arn = aws_iam_policy.eks-cloudwatch-policy.arn
 }
 
-resource "kubectl_manifest" "cloudwatch-fluent-d" {
-  count      = var.enable_cloudwatch_agent ? 1 : 0
-  depends_on = [kubernetes_namespace.amazon-cloudwatch, kubernetes_config_map.cluster-info, aws_iam_role_policy_attachment.fluentd-cloudwatch[0]]
-  yaml_body = templatefile("${path.module}/yamls/cloudwatch-fluentd.yaml", {
+resource "template_file" "cloudwatch-fluentd" {
+  count    = var.enable_cloudwatch_agent ? 1 : 0
+  template = file("${path.module}/yamls/cloudwatch-fluentd.yaml")
+  vars     = {
     account_id            = var.account_id,
     fluentd_iam_role_name = aws_iam_role.fluentd-cloudwatch[0].name,
-  })
+  }
+}
+
+resource "kubectl_manifest" "cloudwatch-fluent-d" {
+  for_each   = split("---", template_file.cloudwatch-fluentd.rendered)
+  depends_on = [kubernetes_namespace.amazon-cloudwatch, kubernetes_config_map.cluster-info, aws_iam_role_policy_attachment.fluentd-cloudwatch[0]]
+  yaml_body  = each.value
 }
