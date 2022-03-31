17f229d4138defd6a82a58187437a464aaa5a9a0
Glaaj <83582518+Glaaj@users.noreply.github.com>
2022-03-31 10:14:01 +0200

    Fix names for log groups

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -5,25 +5,25 @@
 
 resource "aws_cloudwatch_log_group" "cloudwatch-application" {
   count             = var.enable_cloudwatch_agent ? 1 : 0
-  name              = "/aws/containerinsights/${var.eks_cluster_name}/${each.value}"
+  name              = "/aws/containerinsights/${var.eks_cluster_name}/application"
   retention_in_days = var.log_retention_in_days
 }
 
 resource "aws_cloudwatch_log_group" "cloudwatch-dataplane" {
   count             = var.enable_cloudwatch_agent ? 1 : 0
-  name              = "/aws/containerinsights/${var.eks_cluster_name}/${each.value}"
+  name              = "/aws/containerinsights/${var.eks_cluster_name}/dataplane"
   retention_in_days = var.log_retention_in_days
 }
 
 resource "aws_cloudwatch_log_group" "cloudwatch-host" {
   count             = var.enable_cloudwatch_agent ? 1 : 0
-  name              = "/aws/containerinsights/${var.eks_cluster_name}/${each.value}"
+  name              = "/aws/containerinsights/${var.eks_cluster_name}/host"
   retention_in_days = var.log_retention_in_days
 }
 
 resource "aws_cloudwatch_log_group" "cloudwatch-performance" {
   count             = var.enable_cloudwatch_agent ? 1 : 0
-  name              = "/aws/containerinsights/${var.eks_cluster_name}/${each.value}"
+  name              = "/aws/containerinsights/${var.eks_cluster_name}/performance"
   retention_in_days = var.log_retention_in_days
 }
 
