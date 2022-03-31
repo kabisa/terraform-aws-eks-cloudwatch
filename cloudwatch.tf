04a6f870746fbcb30f92b411cdc4de50816fa9b3
Glaaj <83582518+Glaaj@users.noreply.github.com>
2022-03-31 10:02:39 +0200

    Added count to all log groups
    The for each loop conflicts with the count that was used by fluentbit.
    They now behave in the same way.

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -1,11 +1,32 @@
-resource "aws_cloudwatch_log_group" "container_insights" {
-  for_each = toset(["application", "dataplane", "host", "performance"])
+# These log groups are used by cloudwatch and fluentbit,
+# by creating it here we do not have to specify the retention 
+# inside the helm chart. Added benefit is that we can destroy 
+# it using terraform which we wouldn't be able to when using helm
+
+resource "aws_cloudwatch_log_group" "cloudwatch-application" {
+  count             = var.enable_cloudwatch_agent ? 1 : 0
   name              = "/aws/containerinsights/${var.eks_cluster_name}/${each.value}"
   retention_in_days = var.log_retention_in_days
 }
 
-# this log group is specifically used by fluentbit, by creating it here we do not have to specify the retention inside the helm chart
-# Added benefit is that we can destroy it using terraform which we wouldn't be able to when using helm
+resource "aws_cloudwatch_log_group" "cloudwatch-dataplane" {
+  count             = var.enable_cloudwatch_agent ? 1 : 0
+  name              = "/aws/containerinsights/${var.eks_cluster_name}/${each.value}"
+  retention_in_days = var.log_retention_in_days
+}
+
+resource "aws_cloudwatch_log_group" "cloudwatch-host" {
+  count             = var.enable_cloudwatch_agent ? 1 : 0
+  name              = "/aws/containerinsights/${var.eks_cluster_name}/${each.value}"
+  retention_in_days = var.log_retention_in_days
+}
+
+resource "aws_cloudwatch_log_group" "cloudwatch-performance" {
+  count             = var.enable_cloudwatch_agent ? 1 : 0
+  name              = "/aws/containerinsights/${var.eks_cluster_name}/${each.value}"
+  retention_in_days = var.log_retention_in_days
+}
+
 resource "aws_cloudwatch_log_group" "fluentbit" {
   count             = var.enable_fluentbit ? 1 : 0
   name              = "/aws/eks/fluentbit-cloudwatch/logs"
