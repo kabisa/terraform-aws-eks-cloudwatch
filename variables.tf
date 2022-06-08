b52ed8092f49ed529a69200a5029070274192657
Glaaj <83582518+Glaaj@users.noreply.github.com>
2022-06-08 11:42:46 +0200

    Ad description to vars

--- variables.tf
+++ variables.tf
@@ -1,5 +1,6 @@
 variable "account_id" {
   type = string
+  description = "The AWS account ID"
 }
 
 variable "oidc_host_path" {
@@ -8,18 +9,22 @@ variable "oidc_host_path" {
 
 variable "region" {
   type = string
+  description = "The AWS region to deploy to"
 }
 
 variable "enable_cloudwatch_agent" {
   type = bool
+  description = "Boolean to enable cloudwatch agent"
 }
 
 variable "enable_fluentbit" {
   type = bool
+  description = "Boolean to enable fluentbit"
 }
 
 variable "eks_cluster_name" {
   type = string
+  description = "Name of the EKS cluster"
 }
 
 variable "log_retention_in_days" {
